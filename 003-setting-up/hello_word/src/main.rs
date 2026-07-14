f9d782d04e547a18a82db52cf93575ea1210bb09
0tuedon <tuedonmofe@gmail.com>
2026-07-14 14:26:30 +0100

    chore: hands-on with HAMZA

---
+++ 003-setting-up/hello_word/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let my_name = "Tuedon";
+    println!("Hello, {}!", my_name);
+    let age: i32 = 25; // i32 is a 32 bit signed integer
+    let temperature: f64 = 20.5; // floating point 32 bit
+    let is_active: bool = true; // boolean
+    let initial: char = 'A'; // character
+    let count: u32 = 100; // u32 is a 32bit unsigned integer
+    let distance = 15.0; //
+}
